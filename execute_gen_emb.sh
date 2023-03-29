d5588c57072379a537e94a53c58292e795067b0e
francescazfl <francesca.zf.l@berkeley.edu>
2023-03-29 09:40:59 -0700

    add sklearn sh file

--- execute_gen_emb.sh
+++ execute_gen_emb.sh
@@ -1,41 +1,60 @@
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+# queue for generating and sorting all embs
 
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+### GB1 ###
+
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_600k" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_38M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_76M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# TODO RERUN THIS ONE #####
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/sampled.csv" --encoder_name="carp_640M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
 
 
 
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_600k" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
 
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_38M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
 
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.25 --flatten_emb="mean" --seq_end_idx=56
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_76M" --checkpoint=0.125 --flatten_emb="mean" --seq_end_idx=56
 
-python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
+# python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --reset_param=True
 python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56 --resample_param=True
 python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_640M" --checkpoint=1 --flatten_emb="mean" --seq_end_idx=56
 python run_pregen_emb.py --dataset_path="data/proeng/gb1/low_vs_high.csv" --encoder_name="carp_640M" --checkpoint=0.5 --flatten_emb="mean" --seq_end_idx=56
