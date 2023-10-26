23d53fbfc8a8c5ad2236b9b22a2d07c70b70f4e5
francescazfl <francesca.zf.l@berkeley.edu>
2023-10-26 12:09:40 -0700

    update legend

--- scr/vis/res_vis.py
+++ scr/vis/res_vis.py
@@ -8,6 +8,10 @@ import os
 import numpy as np
 import pandas as pd
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
 import seaborn as sns
 import holoviews as hv
 from holoviews import dim
@@ -16,7 +20,14 @@ hv.extension("bokeh")
 
 from scr.vis.vis_utils import BokehSave
 from scr.params.emb import MODEL_SIZE
-from scr.params.vis import ORDERED_TASK_LIST, TASK_LEGEND_MAP, TASK_COLORS
+from scr.params.vis import (
+    ORDERED_TASK_LIST,
+    TASK_LEGEND_MAP,
+    TASK_COLORS,
+    TASK_SIMPLE_COLOR_MAP,
+    PLOT_EXTS,
+)
+
 
 class PlotLayerDelta:
     """
@@ -71,7 +82,7 @@ class PlotLayerDelta:
         # add pre-train degree to that
 
         # now plot and save
-        delta_plot = plot_layer_delta(
+        delta_plot = plot_layer_delta_plt(
             df=slice_df,
             layer_cut=layer_cut,
             arch=arch,
@@ -136,7 +147,97 @@ def delta_layer(layer_cut: int, value_array: np.array) -> np.array:
     return np.array([layer_perf - value_array[0], value_array[-1] - layer_perf])
 
 
-def plot_layer_delta(
+def plot_layer_delta_plt(
+    df: pd.DataFrame,
+    layer_cut: int,
+    arch: str,
+    metric: str,
+    path2folder: str = "results/summary",
+):
+    """A function for plotting and saving layer delta"""
+
+    plot_title = f"{arch.upper()} layer {metric} at x = {layer_cut}"
+
+    print(f"Plotting {plot_title}...")
+
+    if arch == "esm":
+        alaph_values = [0.8]
+        alpha_unique = alaph_values
+        alpha_label = ["1"]
+        size_legend_label = list(MODEL_SIZE.keys())[:4]
+    else:
+        alaph_values = df["ptp"].values
+        alpha_unique = list(df["ptp"].unique())
+        alpha_label = [str(a) for a in alpha_unique]
+        size_legend_label = list(MODEL_SIZE.keys())[-4:]
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(10, 7.5)
+
+    for (task, c) in TASK_SIMPLE_COLOR_MAP.items():
+        sliced_df = df[df["task"] == task]
+        x = sliced_df["x-0"].values
+        y = sliced_df["f-x"].values
+        s = np.log(sliced_df["model_size"].values + 1) * 18
+        scatter = ax.scatter(
+            x, y, c=c, s=s, label=task, alpha=alaph_values, edgecolors="none"
+        )
+
+    # add colored task legend
+    ax.add_artist(ax.legend(title="Tasks", bbox_to_anchor=(1, 1.012), loc="upper left"))
+
+    # add size legend
+    handles, labels = scatter.legend_elements(prop="sizes", color="k", alpha=0.8)
+    legend2 = ax.legend(
+        handles,
+        list(MODEL_SIZE.keys())[:4],
+        bbox_to_anchor=(1, 0.5925),
+        loc="upper left",
+        title="Model sizes",
+    )
+    ax.add_artist(legend2)
+
+    # add alpha legend
+    alpha_legend = [None] * len(alpha_unique)
+
+    for i, (a_value, a_lable) in enumerate(zip(alpha_unique, alpha_label)):
+        alpha_legend[i] = Line2D(
+            [0],
+            [0],
+            marker="o",
+            color="w",
+            label=a_lable,
+            alpha=a_value,
+            markerfacecolor="k",
+            markersize=10,
+        )
+
+    ax.add_artist(
+        ax.legend(
+            handles=alpha_legend,
+            bbox_to_anchor=(1, 0.39),
+            loc="upper left",
+            title="Pretraining degree",
+        )
+    )
+
+    plt.xlabel("x-0")
+    plt.ylabel("f-x")
+    plt.title(plot_title)
+
+    path2folder = os.path.normpath(path2folder)
+
+    print(f"Saving to {path2folder}...")
+
+    for ext in PLOT_EXTS:
+        plot_title_no_space = plot_title.replace(" ", "_")
+        plt.savefig(
+            os.path.join(path2folder, f"{plot_title_no_space}{ext}"),
+            bbox_inches="tight",
+        )
+
+
+def plot_layer_delta_hv(
     df: pd.DataFrame,
     layer_cut: int,
     arch: str,
@@ -153,13 +254,9 @@ def plot_layer_delta(
         alpha = 0.8
     else:
         alpha = "ptp"
-    
+
     delta_scatter = hv.render(
-        hv.Scatter(
-            df,
-            kdims=["x-0"],
-            vdims=["f-x", "task", "model_size", "ptp"],
-        ).opts(
+        hv.Scatter(df, kdims=["x-0"], vdims=["f-x", "task", "model_size", "ptp"],).opts(
             color="task",
             cmap={
                 l: c
@@ -174,7 +271,7 @@ def plot_layer_delta(
             height=400,
             legend_position="right",
             legend_offset=(1, 0),
-            size=np.log(dim("model_size")+1) * 1.5,
+            size=np.log(dim("model_size") + 1) * 1.5,
             title=plot_title,
         )
     )  # * hv.Curve([[0, 0], [0.15, 0.15]]).opts(line_dash="dotted", color="gray")
@@ -188,5 +285,5 @@ def plot_layer_delta(
         bokeh_plot=delta_scatter,
         path2folder=path2folder,
         plot_name=plot_title,
-        plot_width = 800,
+        plot_width=800,
     )
